b0bf07b1fa28ddbd5b3a0de788ba62aee14aeded
Oleksandr Galinski <san9mba@outlook.com>
2022-06-30 23:27:30 +0300

    initial commit

---
+++ CalendarDemo/wwwroot/js/calendar_data.js
@@ -0,0 +1,71 @@
+﻿var CalendarModels = {
+    shift: {
+        1: {
+            Title: "Morning",
+            From: "7:00",
+            To: "19:00",
+            Color: "#0d6efd"
+        },
+        2: {
+            Title: "Evening",
+            From: "15:00",
+            To: "22:00",
+            Color: "#146c43"
+        }
+    },
+    physician: {
+        1: {
+            FirstName: "Michael",
+            LastName: "Thompson"
+        },
+        2: {
+            FirstName: "Alice",
+            LastName: "Madison"
+        }
+    }
+},
+    MockData = [{
+        Date: "07/03/2022",
+        physicianId: 1,
+        shiftId: 2
+    },
+    {
+        Date: "07/03/2022",
+        physicianId: 2,
+        shiftId: 1
+    },
+    {
+        Date: "07/04/2022",
+        physicianId: 1,
+        shiftId: 2
+    },
+    {
+        Date: "07/04/2022",
+        physicianId: 2,
+        shiftId: 1
+        },
+        {
+            Date: "07/14/2022",
+            physicianId: 2,
+            shiftId: 1
+        }
+    ];
+
+function getData() {
+    var calendarData = [];
+    for (var i = 0; i < MockData.length; i++) {
+        var d = MockData[i],
+            date = d.Date,
+            shift = CalendarModels.shift[d.shiftId],
+            physician = CalendarModels.physician[d.physicianId];
+        calendarData.push({
+            Id: i,
+            Subject: shift.Title,
+            PhysicianName: `${physician.FirstName} ${physician.LastName}`,
+            CategoryColor: shift.Color,
+            StartTime: new Date(`${date} ${shift.From}`),
+            EndTime: new Date(`${date} ${shift.To}`),
+        });
+    }
+    return calendarData;
+}
---
+++ CalendarDemo/wwwroot/js/calendar_demo.js
@@ -0,0 +1,30 @@
+﻿function init() {
+    var data = getData();
+    var scheduleObj = new ej.schedule.Schedule({
+        width: '100%',
+        height: `${window.innerHeight -20}px`,
+        currentView: "Month",
+        selectedDate: new Date(2022, 6, 3),
+        eventSettings: {
+            dataSource: data,
+            template: '#apptemplate'
+        },
+        eventRendered: function (args,b,e) {
+            var cellHeight = (scheduleObj.element.querySelector('.e-work-cells')).offsetHeight;
+            var appHeight = cellHeight / 2 - 10;
+            $($(args.element).find('.subject')).css('font-size', '16px');
+            $($(args.element).find('.time')).css('font-size', '24px');
+            args.element.style.height = appHeight + 'px';
+        }
+    });
+    scheduleObj.appendTo('#Schedule');
+    return scheduleObj;
+};
+$(document).ready(function () {
+    var scheduleObj = init();
+    console.log(getData());
+
+    $('#print-btn').click(function() {
+        scheduleObj.print();
+    });
+});
